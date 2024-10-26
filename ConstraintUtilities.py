b2d93eb40418d886ccc2b5a3c69730993113199a
samuel <65369878+SamTheMan122@users.noreply.github.com>
2024-10-26 16:10:06 -0700

    Update ConstraintUtilities.py

--- ConstraintUtilities.py
+++ ConstraintUtilities.py
@@ -51,7 +51,7 @@ def select_general(cls):
     the values of the selected uniqueness constraint for the collection corresponding to that class.
     :param cls: The class that the user wants a single instance of.
     :return: The instance that the user selected."""
-    # If we were doing this directly in mongodb, we'd say collection=db[whatever collection it is]
+    # directly in mongodb, we'd say collection=db[whatever collection it is]
     collection = cls._get_collection()
     index_info = collection.index_information()
     # Loop through the indexes of the collection.
@@ -64,28 +64,24 @@ def select_general(cls):
             choices.append(Option(f'index: {index} - cols: {columns}', index))
     index_menu = Menu('which index', 'Which index do you want to search by:', choices)
     while True:
-        # What happens if there are no unique indexes at all?
         chosen_index = index_menu.menu_prompt()
         filters = {}  # The attribute/value pairs that we're going to search by
         for column in [col[0] for col in index_info[chosen_index]['key']]:
-            # now I have to convert from the column name to the attribute name.
             attribute_name = get_attr_from_column(cls, column)
             # If this attribute is a reference, we need to go find that referenced document.
             attribute = getattr(cls, attribute_name)
             if type(attribute).__name__ == 'ReferenceField':
                 # This attribute in this uniqueness requirement is a reference --> it's an identifying relationship.
-                # Now we have to figure out the class that we're referencing.
+                # figure out the class that we're referencing.
                 referenced_class = attribute.document_type
-                # Use my general selection utility to find an instance of the referenced parent
+                # find an instance of the referenced parent
                 target = select_general(referenced_class)
-                # and add the filter to point to the selected document in the parent collection.
-                # I could have just said: filters[attribute_name] = select_general(attribute.document_type)
+                # add the filter to point to the selected document in the parent collection.
                 filters[attribute_name] = target
             elif type(attribute).__name__ == 'DateTimeField':
                 filters[attribute_name] = prompt_for_date(f'search for {attribute_name} =:')
             else:
-                # It's not a reference, so prompt for the literal value.
-                # This works for string and numeric, but honestly, I should convert the string depending on the type.
+                # prompt for the literal value.
                 filters[attribute_name] = input(f'search for {attribute_name} = --> ')
         # count the number of rows that meet that criteria.
         if cls.objects(**filters).count() == 1:
@@ -104,7 +100,6 @@ def unique_general(instance):
                         uniqueness constraints on that collection.
     :return:            A list of the 0 or more uniqueness constraints that have been violated.
     """
-    # If we were doing this directly in mongodb, we'd say collection=db[whatever collection it is]
     cls = instance.__class__  # get the class from the instance.
     collection = cls._get_collection()  # Get the collection corresponding to that class.
     index_info = collection.index_information()  # get a list of all indexes on that collection.
@@ -113,19 +108,15 @@ def unique_general(instance):
     violated_constraints = []
     for index in index_info.keys():  # Loop through the list of index names.
         # see if the index is unique.  If not, we're not interested in using it.
-        # _id_ does not have a property named unique since _id_ is ALWAYS unique.
-        # Normally, _id_ is assigned by MongoDB, but the user COULD use that for a descriptive
-        # attribute, which COULD mean that there is a document with that _id_ value already.
         if index == '_id_' or index_info[index]['unique']:
             # harvest the list of column names from this index.
             columns = [col[0] for col in index_info[index]['key']]
             # add the name of the index & the column list to our constraints list of dictionaries.
             constraints.append({'name': index, 'columns': columns})
     for constraint in constraints:
-        # What happens if there are no unique indexes at all?
         filters = {}  # The attribute/value pairs that we're going to search by
         for column in constraint['columns']:
-            # now I have to convert from the column name to the attribute name.
+            # convert from the column name to the attribute name.
             attribute_name = get_attr_from_column(cls, column)
             # Add the next key=value pair to our list of filters.
             filters[attribute_name] = instance[attribute_name]
@@ -134,5 +125,4 @@ def unique_general(instance):
             # there is a document in the collection that matches the supplied instance on all attributes
             # of this uniqueness constraint.  So we have another uniqueness constraint violation.
             violated_constraints.append(constraint)
-    # If the returned list of violated constraints == [], we know that we are good to insert this object.
     return violated_constraints
